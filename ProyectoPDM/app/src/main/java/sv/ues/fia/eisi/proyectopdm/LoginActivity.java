7e3d980beaa9d52ff8183f34e27cc7becbfa4a87
abcgv <vabcgv@outlook.com>
2020-06-06 01:03:12 -0600

    Cambio de activity de lanzamiento inicial

--- ProyectoPDM/app/src/main/java/sv/ues/fia/eisi/proyectopdm/LoginActivity.java
+++ ProyectoPDM/app/src/main/java/sv/ues/fia/eisi/proyectopdm/LoginActivity.java
@@ -1,14 +1,26 @@
 package sv.ues.fia.eisi.proyectopdm;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText email, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionBar= getSupportActionBar();
+        actionBar.hide();
+        email = (EditText) findViewById(R.id.etEmail);
+        pass = (EditText) findViewById(R.id.etPassword);
+    }
+    public void Login(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
