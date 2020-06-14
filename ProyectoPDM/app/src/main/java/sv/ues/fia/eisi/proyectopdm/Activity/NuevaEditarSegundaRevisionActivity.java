b34123f6e66f362663d313024e993df20032813f
rubper <49503500+rubper@users.noreply.github.com>
2020-06-14 02:56:52 -0600

    pequeño fix segunda revision

--- ProyectoPDM/app/src/main/java/sv/ues/fia/eisi/proyectopdm/Activity/NuevaEditarSegundaRevisionActivity.java
+++ ProyectoPDM/app/src/main/java/sv/ues/fia/eisi/proyectopdm/Activity/NuevaEditarSegundaRevisionActivity.java
@@ -14,6 +14,9 @@ import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Locale;
+import java.util.Objects;
+
 import sv.ues.fia.eisi.proyectopdm.R;
 import sv.ues.fia.eisi.proyectopdm.ViewModel.PrimeraRevisionViewModel;
 import sv.ues.fia.eisi.proyectopdm.ViewModel.SegundaRevisionViewModel;
@@ -25,7 +28,8 @@ public class NuevaEditarSegundaRevisionActivity extends AppCompatActivity {
 
     private int idPrimera;
 
-    private EditText editIdSegundaRevision;
+    private SegundaRevision segundaRevisionActual;
+
     private EditText editNotaFinalSegundaRevision;
     private EditText editObservacionesSegundaRevision;
     private DatePicker dpickFechaSolicitudSegundaRevision;
@@ -44,7 +48,6 @@ public class NuevaEditarSegundaRevisionActivity extends AppCompatActivity {
             final Bundle extras = getIntent().getExtras();
 
             //inicialización de elementos del layout
-            editIdSegundaRevision = findViewById(R.id.edit_id_segundarevision);
             editNotaFinalSegundaRevision = findViewById(R.id.edit_notafinal_segundarevision);
             editObservacionesSegundaRevision = findViewById(R.id.edit_observaciones_segundarevision);
             dpickFechaSolicitudSegundaRevision = findViewById(R.id.edit_fecha_solicitud_segundarevision);
@@ -52,7 +55,7 @@ public class NuevaEditarSegundaRevisionActivity extends AppCompatActivity {
             dpickFechaSegundaRevision = findViewById(R.id.edit_fecha_segundarevision);
 
             //nomeacuerdoxdddddd
-            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+            Objects.requireNonNull(getSupportActionBar()).setHomeAsUpIndicator(R.drawable.ic_close);
 
             //PARA EDITAR
             //instancia View Model de segundaRevision
@@ -66,30 +69,38 @@ public class NuevaEditarSegundaRevisionActivity extends AppCompatActivity {
                 if(operacionEv == EditarPrimeraRevisionActivity.EDITAR_SEGUNDA_REVISION){
                     //obtener segundaRevision auxiliar
                     auxiliar = segundaRevisionViewModel.getSegundaRevision(idPrimeraRevision);
+                    //asigna id de primera revision a atributo de clase
                     idPrimera = idPrimeraRevision;
-                    //obtener primeraRevision actual
-                    PrimeraRevision primeraRevisionActual = primeraRevisionViewModel.getPrimeraRevision(auxiliar.getIdPrimeraRevisionFK());
-                    //settear editTexts con los objetos obtenidos
-                    editIdSegundaRevision.setText(auxiliar.getIdSegundaRevision());
-                    //convertir a string
-                    editNotaFinalSegundaRevision.setText(String.format("%s", auxiliar.getNotaFinalSegundaRev()));
+                    segundaRevisionActual = auxiliar;
+                    //colocar nota obtenida en edittext
+                    editNotaFinalSegundaRevision.setText(String.format(Locale.US, "%.2f", auxiliar.getNotaFinalSegundaRev()));
+                    //colocar observación obtenida en edittext
                     editObservacionesSegundaRevision.setText(auxiliar.getObservacionesSegundaRev());
                     //separa en array la fecha obtenida
                     String[] fechaSolicitud = auxiliar.getFechaSolicitudSegRev().split("/");
                     //se ingresa la fecha desde el array
                     dpickFechaSolicitudSegundaRevision.init(Integer.parseInt(fechaSolicitud[2]),Integer.parseInt(fechaSolicitud[1]),Integer.parseInt(fechaSolicitud[0]), null);
+                    //separa la fecha de revision en un array
                     String[] fechaRevision = auxiliar.getFechaSegundaRev().split("/");
+                    //se coloca la fecha en el dpick desde array
                     dpickFechaSegundaRevision.init(Integer.parseInt(fechaRevision[2]),Integer.parseInt(fechaRevision[1]),Integer.parseInt(fechaRevision[0]), null);
+                    //se separa la hora de revision en un array
                     String[] horaRevision = auxiliar.getFechaSegundaRev().split(":");
+                    //se coloca la hora desde el array
                     tpickHoraSegundaRevision.setCurrentHour(Integer.parseInt(horaRevision[0]));
+                    //se colocan los minutos
                     tpickHoraSegundaRevision.setCurrentMinute(Integer.parseInt(horaRevision[1]));
+                    //título de activity
                     setTitle(R.string.titulo_EA_editarsegrev);
+                    //se activan los campos nota y observaciones
                     editNotaFinalSegundaRevision.setVisibility(View.VISIBLE);
                     editObservacionesSegundaRevision.setVisibility(View.VISIBLE);
                 }
                 else if (operacionEv == EditarPrimeraRevisionActivity.AÑADIR_SEGUNDA_REVISION){
+                    //se desactivan los campos nota y observaciones
                     editNotaFinalSegundaRevision.setVisibility(View.GONE);
                     editObservacionesSegundaRevision.setVisibility(View.GONE);
+                    //título de activity
                     setTitle(R.string.titulo_EA_nuevasegrev);
                 }
             }
@@ -102,76 +113,42 @@ public class NuevaEditarSegundaRevisionActivity extends AppCompatActivity {
 
     private void guardarSegundaRevision() {
         try {
-            //---alamcenar NOMBRE segundaRevision
-            String nombre = editIdSegundaRevision.getText().toString();
-
-            //---obtener PRimeraRevision
-            PrimeraRevision primeraRevision = primeraRevisionViewModel.getPrimeraRevision(idPrimera);
-
-            //---obtener valor de datepicker FECHA REVISION segundaRevision
-            StringBuilder fechaRevisionBuilder = new StringBuilder(10);
-            //concatenar
-            fechaRevisionBuilder.append(dpickFechaSegundaRevision.getDayOfMonth()).append("/").append(dpickFechaSegundaRevision.getMonth()).append("/").append(dpickFechaSegundaRevision.getYear());
             //almacenar FECHA REVISION
-            String fechaRevision = fechaRevisionBuilder.toString();
-
-            //---obtener valor de datepicker FECHA SOLICITUD segundaRevision
-            StringBuilder fechaSolicitudBuilder = new StringBuilder(10);
-            //concatenar
-            fechaSolicitudBuilder.append(dpickFechaSolicitudSegundaRevision.getDayOfMonth()).append("/").append(dpickFechaSolicitudSegundaRevision.getMonth()).append("/").append(dpickFechaSolicitudSegundaRevision.getYear());
+            String fechaRevision = String.format("%s/%s/%s", dpickFechaSegundaRevision.getDayOfMonth(), dpickFechaSegundaRevision.getMonth(), dpickFechaSegundaRevision.getYear());
             //almacenar FECHA SOLICITUD
-            String fechaSolicitud = fechaSolicitudBuilder.toString();
-
-            //---obtener valor de timepicker HORA segundaRevision
-            StringBuilder horaBuilder = new StringBuilder(10);
-            //concatenar
-            horaBuilder.append(tpickHoraSegundaRevision.getCurrentHour()).append(":").append(tpickHoraSegundaRevision.getCurrentMinute());
+            String fechaSolicitud = String.format("%s/%s/%s",dpickFechaSolicitudSegundaRevision.getDayOfMonth(), dpickFechaSolicitudSegundaRevision.getMonth(), dpickFechaSolicitudSegundaRevision.getYear());
             //almacenar HORA
-            String hora = horaBuilder.toString();
-
-            if(nombre.trim().isEmpty()){
-                Toast.makeText(this,getText(R.string.error_form_incompleto_eval), Toast.LENGTH_LONG).show();
-                return;
-            }
-            //instancia View Model de segundaRevision
-            segundaRevisionViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(SegundaRevisionViewModel.class);
+            String hora = String.format("%s:%s",tpickHoraSegundaRevision.getCurrentHour(), tpickHoraSegundaRevision.getCurrentMinute());
 
             //obtener extras del intent
             Bundle extras = getIntent().getExtras();
-            int idPrimeraRevision = 0, operacionEv = 0;
+            int operacionEv = 0;
             if (extras != null) {
-                idPrimeraRevision = extras.getInt(EditarPrimeraRevisionActivity.IDENTIFICADOR_PRIMERA_REVISION);
                 operacionEv = extras.getInt(EditarPrimeraRevisionActivity.OPERACION_SEGUNDA_REVISION);
                 //verificar extras de intent
                 if (operacionEv == EditarPrimeraRevisionActivity.EDITAR_SEGUNDA_REVISION) {
-
                     //---almacenar NOTA FINAL
                     String notaFinal = editNotaFinalSegundaRevision.getText().toString();
 
                     //---almacenar OBSERVACIONES
-                    String observaciones = editObservacionesSegundaRevision.getText().toString();
+                    String observaciones = editObservacionesSegundaRevision.getText().toString().trim();
 
                     //Objeto SegRevuación auxiliar construido a partir de los datos almacenados
-                    SegundaRevision aux = new SegundaRevision(idPrimeraRevision,fechaRevision,hora,Double.parseDouble(notaFinal),observaciones,fechaSolicitud);
-
+                    SegundaRevision aux = new SegundaRevision(idPrimera,fechaRevision,hora,Double.parseDouble(notaFinal),observaciones,fechaSolicitud);
+                    aux.setIdSegundaRevision(segundaRevisionActual.getIdSegundaRevision());
                     //insertar
                     segundaRevisionViewModel.updateSegundaRevision(aux);
                     //mensaje de éxito (si falla, el try lo atrapa y en vez de mostrar este toast, muestra el toast con la excepción más abajo)
-                    Toast.makeText(NuevaEditarSegundaRevisionActivity.this, getText(R.string.inic_notif_segrev) + nombre + getText(R.string.accion_actualizar_notif_eval), Toast.LENGTH_LONG).show();
+                    Toast.makeText(NuevaEditarSegundaRevisionActivity.this, String.format("%s%s%s",getText(R.string.inic_notif_segrev), String.format("%d",aux.getIdSegundaRevision()), getText(R.string.accion_actualizar_notif_eval)), Toast.LENGTH_LONG).show();
                 } else if (operacionEv == EditarPrimeraRevisionActivity.AÑADIR_SEGUNDA_REVISION ) {
-                    //---almacenar OBSERVACIONES
-                    String observaciones = editObservacionesSegundaRevision.getText().toString();
 
                     //Objeto SegRevuación auxiliar construido a partir de los datos almacenados
                     SegundaRevision aux;
-                    if(observaciones.isEmpty())
-                        aux = new SegundaRevision(idPrimeraRevision,fechaRevision,hora,fechaSolicitud);
-                    else
-                        aux = new SegundaRevision(idPrimeraRevision,fechaRevision,hora,observaciones,fechaSolicitud);
+                    aux = new SegundaRevision(idPrimera,fechaRevision,hora,fechaSolicitud);
                     //insertar
                     segundaRevisionViewModel.insertSegundaRevision(aux);
                     //mensaje de éxito (si falla, el try lo atrapa y en vez de mostrar este toast, muestra el toast con la excepción más abajo)
-                    Toast.makeText(NuevaEditarSegundaRevisionActivity.this, getText(R.string.inic_notif_segrev) + nombre  + getText(R.string.accion_insertar_notif_eval), Toast.LENGTH_LONG).show();
+                    Toast.makeText(NuevaEditarSegundaRevisionActivity.this,String.format("%s%s", getText(R.string.inic_notif_segrev) , getText(R.string.accion_insertar_notif_eval)), Toast.LENGTH_LONG).show();
                 }
             }
             //salir de la actividad
