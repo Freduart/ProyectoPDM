e153556e79eb900dd57da816cf7a56d8a4de4d74
Fredy Martinez <afredxxstartrekxx@gmail.com>
2020-06-17 18:31:07 -0600

    Cambios en editar
    Se corrigio errores en editar alumno

--- ProyectoPDM/app/src/main/java/sv/ues/fia/eisi/proyectopdm/Activity/EditarEncargadoImpresionActivity.java
+++ ProyectoPDM/app/src/main/java/sv/ues/fia/eisi/proyectopdm/Activity/EditarEncargadoImpresionActivity.java
@@ -77,7 +77,7 @@ public class EditarEncargadoImpresionActivity extends AppCompatActivity {
             textNomenc.setError("Ingrese El Nombre Del Nuevo Encargado.");
         }else{
             String nomEnc=textNomenc.getEditText().getText().toString();
-            EncargadoImpresion encargadoImpresion=new EncargadoImpresion(nomEnc, 1);
+            EncargadoImpresion encargadoImpresion=new EncargadoImpresion(nomEnc, 4);
             encargadoImpresion.setIdEncargadoImpresion(encargadoImpresionActual.getIdEncargadoImpresion());
             try {
                 encargadoImpresionViewModel=new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(EncargadoImpresionViewModel.class);
