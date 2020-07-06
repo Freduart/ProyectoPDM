2624c730258c61a3fc365780523018e4dc9536ea
abcgv <vabcgv@outlook.com>
2020-07-05 20:31:22 -0600

    Voz a texto en AgregarAlumnoActivity

--- ProyectoPDM/app/src/main/java/sv/ues/fia/eisi/proyectopdm/Activity/VerLocalActivity.java
+++ ProyectoPDM/app/src/main/java/sv/ues/fia/eisi/proyectopdm/Activity/VerLocalActivity.java
@@ -40,21 +40,14 @@ public class VerLocalActivity extends AppCompatActivity {
     private TextView dispUbLocal;
     private TextView dispLatLocal;
     private TextView dispLonLocal;
-    //para geolocalización
-    private FusedLocationProviderClient fusedLocationClient;
-    private int MY_PERMISSIONS_REQUEST_LOCATION;
+
     private ImageButton mapa;
-    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_ver_local);
-            //para geolocalización
-            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-            mDatabase = FirebaseDatabase.getInstance().getReference();
-            subirLatLongFirebase();
             mapa = (ImageButton) findViewById(R.id.btnMap);
 
             //Título personalizado para Activity
@@ -93,38 +86,6 @@ public class VerLocalActivity extends AppCompatActivity {
         } catch (Exception e) {
             Toast.makeText(this, e.getMessage() + " - " + e.fillInStackTrace().toString(), Toast.LENGTH_LONG).show();
         }
-    }
-
-
-
-    private void subirLatLongFirebase() {
-
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(VerLocalActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_LOCATION);
-
-            return;
-        }
-        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                // Got last known location. In some rare situations this can be null.
-                if (location != null) {
-                    location.setLatitude(localActual.getLatitud());
-                    location.setLongitude(localActual.getLongitud());
-                    Log.e("Latitud: ", + location.getLatitude() + " Longitud: "+ location.getLongitude());
-                    Map<String, Object> latLong = new HashMap<>();
-                    latLong.put("latitud", location.getLatitude());
-                    latLong.put("longitud", location.getLongitude());
-                    mDatabase.child("locales").push().setValue(latLong);
-                }
-            }
-        });
     }
 
     public void redirectMaps(View view){
